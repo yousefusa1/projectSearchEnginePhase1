c042cbbf7b56d9da00b9a74bc9f7af7366c54469
farhangdon <farhanfastian@gmail.com>
2015-02-27 12:20:49 +0500

    FirstCommit

---
+++ src/projectsearchenginephase1/ProjectSearchEnginePhase1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectsearchenginephase1;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Ali
+ */
+public class ProjectSearchEnginePhase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        try { 
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+} catch (Exception e) {
+    e.printStackTrace();
+}
+   
+        searchEngineGUI searchGUI = new searchEngineGUI();
+        searchGUI.setVisible(true);
+    }
+}
